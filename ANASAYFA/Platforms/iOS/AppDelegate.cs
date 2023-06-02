3edd833cb226167ad086c515911e62db59a4a235
x <x@DESKTOP-IKEJT8V>
2023-06-02 22:34:33 +0300

    Proje dosyası ekle.

---
+++ ANASAYFA/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ANASAYFA;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
